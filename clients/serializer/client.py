28015ba74ac7e741e5d357b464e6d527472c7182
juliomenaya <juliomenaya@gmail.com>
2023-05-06 02:01:53 -0600

    client serializer changes

--- clients/serializer/client.py
+++ clients/serializer/client.py
@@ -2,8 +2,17 @@ from rest_framework import serializers
 
 
 class ClientSerializer(serializers.Serializer):
+    client_id = serializers.CharField(max_length=100)  # ID
     name = serializers.CharField(max_length=100)  # NOM
     last_name = serializers.CharField(max_length=100)  # Prénom
     client_type = serializers.CharField(max_length=100)  # Type de client
     country = serializers.CharField(max_length=100)  # Pays
     
+    def to_internal_value(self, data):
+        return {
+            'Num Dossier': data['client_id'],
+            'NOM': data['name'],
+            'Prénom': data['last_name'],
+            'Type de client': data['client_type'],
+            'Pays client': data['country'],
+        }
