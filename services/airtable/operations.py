35d525b46ccb1c85cb1861825b315dc7723a5158
juliomenaya <juliomenaya@gmail.com>
2023-05-07 19:26:27 -0600

    airtable operations

---
+++ services/airtable/operations.py
@@ -0,0 +1,13 @@
+from requests.exceptions import HTTPError
+
+
+class Operations():
+    def __init__(self, base):
+        self.base = base
+
+    @property
+    def table_name(self):
+        raise NotImplementedError
+
+    def operation(self, name, *args, **kwargs):
+        return getattr(self.base, name)(self.table_name, *args, **kwargs)
