4f6a149a26ba50fcce09109c257b11034484ddb2
juliomenaya <juliomenaya@gmail.com>
2023-05-07 19:27:09 -0600

    client serializer

---
+++ clients/serializer/__init__.py
@@ -0,0 +1,5 @@
+from .client import ClientSerializer
+
+__all__ = [
+    'ClientSerializer',
+]
--- clients/serializer/client.py
+++ clients/serializer/client.py
@@ -5,14 +5,13 @@ class ClientSerializer(serializers.Serializer):
     client_id = serializers.CharField(max_length=100)  # ID
     name = serializers.CharField(max_length=100)  # NOM
     last_name = serializers.CharField(max_length=100)  # Prénom
-    client_type = serializers.CharField(max_length=100)  # Type de client
-    country = serializers.CharField(max_length=100)  # Pays
+    country = serializers.CharField(max_length=100, required=False)  # Pays
     
     def to_internal_value(self, data):
         return {
             'Num Dossier': data['client_id'],
             'NOM': data['name'],
             'Prénom': data['last_name'],
-            'Type de client': data['client_type'],
-            'Pays client': data['country'],
+            'Type de client': 'B2B',
+            'Pays client': data.get('country'),
         }
