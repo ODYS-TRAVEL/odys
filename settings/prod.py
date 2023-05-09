ab251e32d508130fb62bf88d98d8aba7475c4cb5
juliomenaya <juliomenaya@gmail.com>
2023-05-08 20:21:10 -0600

    procfile and adding setting to read database url

--- settings/prod.py
+++ settings/prod.py
@@ -1,9 +1,12 @@
 from .common import *
 
 import os
+import dj_database_url
 
 
 AIRTABLE_API_TOKEN = os.environ.get('AIRTABLE_API_TOKEN')
 AIRTABLE_BASE_ID = os.environ.get('AIRTABLE_BASE_ID')
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
