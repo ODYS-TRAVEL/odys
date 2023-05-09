fce81700c9d94f34d02e7e1d2cb45ba46279bdc5
juliomenaya <juliomenaya@gmail.com>
2023-05-08 20:31:23 -0600

    allowed hosts to serve static files

--- settings/prod.py
+++ settings/prod.py
@@ -7,6 +7,9 @@ import dj_database_url
 AIRTABLE_API_TOKEN = os.environ.get('AIRTABLE_API_TOKEN')
 AIRTABLE_BASE_ID = os.environ.get('AIRTABLE_BASE_ID')
 
+# read database url from heroku config
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+ALLOWED_HOSTS = ['*']  # allow heroku to serve the website
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
